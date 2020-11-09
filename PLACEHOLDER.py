ba699037a14457e6797beff3d56fa3fba1879db5
Jumperkables <tom.winterbottom@msn.com>
2020-11-09 23:46:32 +0000

    updated the AVSD plots

--- PLACEHOLDER.py
+++ PLACEHOLDER.py
@@ -133,7 +133,7 @@ def normdict2plot(norm_dicts, dict_labels, title="DEFAULT TITLE", xlab="DEFAULT 
     plt.ylabel(ylab)
     plt.title(title)
     plt.xticks(index + bar_width, ('None', '==0', '0-0.2', '0.2-0.4','0.4-0.6','0.6-0.8','0.8-1','1<'))
-    plt.legend()
+    plt.legend(loc=0)
     
     plt.tight_layout()
     #plt.show()
