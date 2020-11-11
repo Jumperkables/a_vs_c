d616d9892e94e27cc846600aca31a2d4dc75bd92
Jumperkables <tom.winterbottom@msn.com>
2020-11-11 02:12:00 +0000

    assoc vs categorical network

--- plots_n_stats/all/3_my_models/README.md
+++ plots_n_stats/all/3_my_models/README.md
@@ -5,14 +5,14 @@ Categorical-vs-Associative Network runs
 DESCRIPTION PENDING
 
 ### TVQA:
-* `scripts/tvqa/runs/my_models/*` e.g. svi.sh
+* `scripts/tvqa/runs/my_models/*/*` e.g. `mk0/svi.sh`
 
 Model       | Word Embeddings   | Accuracy
 :--         | --:               | --:
-Default-SVI | GloVe             | %
-Default-VI  | GloVe             | %
-Default-SVI | BERT              | %
-Default-VI  | BERT              | %
+Default-SVI | GloVe             | 69.34%
+Default-VI  | GloVe             | ??%
+Default-SVI | BERT              | 72.13%
+Default-VI  | BERT              | ??%
 mk0-SVI     | GloVe             | %
 mk0-VI      | GloVe             | %
 mk0-SVI     | BERT              | %
--- scripts/tvqa/runs/my_models/mk0/svi_glove.sh
+++ scripts/tvqa/runs/my_models/mk0/svi_glove.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 #SBATCH -p part0
-#SBATCH --job-name svi_glove_concgt300 
+#SBATCH --job-name mk0_svi_glove 
 #SBATCH --ntasks 6
 #SBATCH --gres gpu:1 
-#SBATCH -o svi_glove_concgt300.out
+#SBATCH -o mk0_svi_glove.out
 
 source /home/jumperkables/kable_management/python_venvs/mk8-tvqa/bin/activate
 
 python -W ignore tvqa_modality_bias/main.py \
     --input_streams sub vcpt imagenet \
-    --jobname=nstpwrds_svi_glove_concgt300 \
-    --results_dir_base=concgt300 \
-    --modelname=tvqa_abc_bert_nofc \
+    --jobname=mk0_svi_glove \
+    --results_dir_base=mk0_svi_glove \
+    --modelname=mk0 \
     --lrtype radam \
     --bsz 32 \
     --log_freq 800 \
     --test_bsz 100 \
     --lanecheck True \
     --word2idx tvqa_modality_bias/data/cache/word2idx.pickle \
-    --lanecheck_path concgt300/lanecheck_dict.pickle 
+    --lanecheck_path mk0_svi_glove/lanecheck_dict.pickle 
