7d21a702c6e67a05e3e1a3c644dd7698d8912aa0
Jumperkables <tom.winterbottom@msn.com>
2020-12-28 17:24:14 +0000

    implemented norm plotting

--- plots_n_stats/norms/6_assoc_clusters/README.md
+++ plots_n_stats/norms/6_assoc_clusters/README.md
@@ -1,3 +1,9 @@
 # Association, and other norm, clusters:
-`assoc_nx.sh`
-![Assoc > 0.7](usf_assoc_gt0-5.png)
+Run this plotting code with: `assoc_nx.sh`
+
+### Assoc > 0.7:
+![Assoc > 0.7](usf_assoc_gt0-7.png)
+
+### Assoc > 0.4:
+![Assoc > 0.4](usf_assoc_gt0-4.png)
+
--- plots_n_stats/norms/6_assoc_clusters/assoc_nx.sh
+++ plots_n_stats/norms/6_assoc_clusters/assoc_nx.sh
@@ -3,11 +3,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ../../../venvs/a_vs_c/bin/activate
 python ../../../misc/norm_analysis.py \
     --norm "assoc" \
-    --norm_threshold 0.7 \
+    --norm_threshold 0.4 \
     \
     \
     --purpose "G_2_nx" \
     --draw_style "default" \
     --save \
-    --save_path "/home/jumperkables/kable_management/projects/a_vs_c/plots_n_stats/norms/6_assoc_clusters/usf_assoc_gt0-5.png" \
-    --title "USF Assoc Metric Network: Assoc > 0.5"
+    --save_path "/home/jumperkables/kable_management/projects/a_vs_c/plots_n_stats/norms/6_assoc_clusters/usf_assoc_gt0-4.png" \
+    --title "USF Assoc Metric Network: Assoc > 0.4"
