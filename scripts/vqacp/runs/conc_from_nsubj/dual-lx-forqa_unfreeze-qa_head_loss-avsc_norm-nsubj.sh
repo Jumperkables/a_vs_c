73a33b8cf6ec74a5402ec71b7510b529d2aaff76
Jumperkables <tom.winterbottom@msn.com>
2021-03-29 02:28:18 +0100

    final run on ncc

--- scripts/vqacp/runs/conc_from_nsubj/dual-lx-forqa_unfreeze-qa_head_loss-avsc_norm-nsubj.sh
+++ scripts/vqacp/runs/conc_from_nsubj/dual-lx-forqa_unfreeze-qa_head_loss-avsc_norm-nsubj.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --qos long-high-prio
+#SBATCH --qos short
 #SBATCH -N 1
 #SBATCH -c 4
-#SBATCH -t 7-00:00
+#SBATCH -t 2-00:00
 #SBATCH --mem 16G
 #SBATCH -p res-gpu-small
 #SBATCH --job-name vqacp-topk-1500_dual-lx-forqa_unfreeze-qa_head_loss-avsc_norm-nsubj 
@@ -24,6 +24,6 @@ python -W ignore VQA_dsets.py \
     --val_bsz 100 \
     --device 0 \
     --unfreeze qa_head \
-    --num_workers 4 \
+    --num_workers 2 \
     --lr 0.00001 \
     --wandb \
