76d2469a412242becd2881cf31603f3a5c3ae394
Jumperkables <tom.winterbottom@msn.com>
2021-03-29 02:26:52 +0100

    sorted things out

--- scripts/gqa/runs/conc_from_nsubj/dual-lx-forqa_unfreeze-qa_head_loss-default_norm-nsubj.sh
+++ scripts/gqa/runs/conc_from_nsubj/dual-lx-forqa_unfreeze-qa_head_loss-default_norm-nsubj.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#SBATCH --qos long-high-prio
+#SBATCH --qos short
 #SBATCH -N 1
 #SBATCH -c 4
-#SBATCH -t 7-00:00
-#SBATCH -x gpu[0-3]
-#SBATCH --mem 12G
+#SBATCH -t 2-00:00
+#SBATCH --mem 16G
 #SBATCH -p res-gpu-small
 #SBATCH --job-name gqa_dual-lx-forqa_unfreeze-qa_head_loss-default_norm-nsubj 
 #SBATCH --gres gpu:1 
