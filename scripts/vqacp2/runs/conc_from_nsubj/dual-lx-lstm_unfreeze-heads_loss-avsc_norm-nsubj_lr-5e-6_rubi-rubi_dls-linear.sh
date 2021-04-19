33aaeb54c6fdda9d5b8043ee7644a727b3ac3aac
Jumperkables <tom.winterbottom@msn.com>
2021-04-19 14:46:49 +0100

    vqacp scripts prepared

---
+++ scripts/vqacp2/runs/conc_from_nsubj/dual-lx-lstm_unfreeze-heads_loss-avsc_norm-nsubj_lr-5e-6_rubi-rubi_dls-linear.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --qos long-high-prio
+#SBATCH -N 1
+#SBATCH -c 4
+#SBATCH -t 7-00:00
+#SBATCH --mem 20G
+#SBATCH -t 7-00:00
+#SBATCH -p res-gpu-small
+#SBATCH --job-name vqacp2_topk-1500_dual-lx-lstm_unfreeze-heads_loss-avsc_norm-nsubj_lr-5e-6_rubi-rubi_dls-linear 
+#SBATCH --gres gpu:1 
+#SBATCH -o vqacp2_topk-1500_dual-lx-lstm_unfreeze-heads_loss-avsc_norm-nsubj_lr-5e-6_rubi-rubi_dls-linear.out
+
+cd ../../../..
+source venvs/a_vs_c/bin/activate
+export PYTHONBREAKPOINT=ipdb.set_trace
+python -W ignore VQA_dsets.py \
+    --jobname vqacp2_topk-1500_dual-lx-lstm_unfreeze-heads_loss-avsc_norm-nsubj_lr-5e-6_rubi-rubi_dls-linear \
+    --dataset VQACP2 \
+    --topk 1500 \
+    --model dual-lx-lstm \
+    --loss avsc \
+    --norm_gt nsubj \
+    --epochs 1000 \
+    --bsz 32 \
+    --val_bsz 100 \
+    --device 0 \
+    --unfreeze heads \
+    --num_workers 2 \
+    --lr 0.000005 \
+    --rubi rubi \
+    --dual_loss_style linear \
+    --wandb \
