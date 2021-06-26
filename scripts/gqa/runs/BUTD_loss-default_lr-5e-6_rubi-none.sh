4242cae45f043049e80d0c5516544cf56d5084bb
Jumperkables <tom.winterbottom@msn.com>
2021-06-26 03:19:14 +0100

    fixed GQA running scripts

--- scripts/gqa/runs/BUTD_loss-default_lr-5e-6_rubi-none.sh
+++ scripts/gqa/runs/BUTD_loss-default_lr-5e-6_rubi-none.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 #SBATCH --ntasks 6
 #SBATCH -p part0
-#SBATCH --job-name gqa_topk-1500_BUTD_loss-default_lr-5e-6_rubi-none 
+#SBATCH --job-name gqa_BUTD_loss-default_lr-5e-6_rubi-none 
 #SBATCH --gres gpu:1 
-#SBATCH -o gqa_topk-1500_BUTD_loss-default_lr-5e-6_rubi-none.out
+#SBATCH -o gqa_BUTD_loss-default_lr-5e-6_rubi-none.out
 
 cd ../../..
 source venvs/a_vs_c/bin/activate
 export PYTHONBREAKPOINT=ipdb.set_trace
 python -W ignore main.py \
-    --jobname gqa_topk-1500_BUTD_loss-default_lr-5e-6_rubi-none \
+    --jobname gqa_BUTD_loss-default_lr-5e-6_rubi-none \
     --dataset gqa \
-    --topk 1500 \
     --model BUTD \
     --loss default \
     --epochs 100 \
--- scripts/gqa/runs/lx-lstm_unfreeze-heads_loss-avsc-scaled_lr-5e-6_rubi-none.sh
+++ scripts/gqa/runs/lx-lstm_unfreeze-heads_loss-avsc-scaled_lr-5e-6_rubi-none.sh
@@ -5,15 +5,15 @@
 #SBATCH -t 2-00:00
 #SBATCH --mem 21G
 #SBATCH -p res-gpu-small
-#SBATCH --job-name gqa_topk-1500_lx-lstm_unfreeze-heads_loss-avsc-scaled_lr-5e-6_rubi-none 
+#SBATCH --job-name gqa_lx-lstm_unfreeze-heads_loss-avsc-scaled_lr-5e-6_rubi-none 
 #SBATCH --gres gpu:1 
-#SBATCH -o gqa_topk-1500_lx-lstm_unfreeze-heads_loss-avsc-scaled_lr-5e-6_rubi-none.out
+#SBATCH -o gqa_lx-lstm_unfreeze-heads_loss-avsc-scaled_lr-5e-6_rubi-none.out
 
 cd ../../..
 source venvs/a_vs_c/bin/activate
 export PYTHONBREAKPOINT=ipdb.set_trace
 python -W ignore main.py \
-    --jobname gqa_topk-1500_lx-lstm_unfreeze-heads_loss-avsc-scaled_lr-5e-6_rubi-none \
+    --jobname gqa_lx-lstm_unfreeze-heads_loss-avsc-scaled_lr-5e-6_rubi-none \
     --dataset gqa \
     --model lx-lstm \
     --loss avsc-scaled \
