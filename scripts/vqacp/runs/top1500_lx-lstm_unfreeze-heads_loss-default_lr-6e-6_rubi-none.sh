1b169111343c68cb029e133dc96b96905bfdcacc
Jumperkables <tom.winterbottom@msn.com>
2021-08-02 15:12:47 +0100

    jobs ready to go

---
+++ scripts/vqacp/runs/top1500_lx-lstm_unfreeze-heads_loss-default_lr-6e-6_rubi-none.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --qos short
+#SBATCH -N 1
+#SBATCH -c 4
+#SBATCH -t 2-00:00
+#SBATCH --mem 18G
+#SBATCH -p res-gpu-small
+#SBATCH --job-name vqacp_topk-1500_lx-lstm_unfreeze-heads_loss-default_lr-6e-6_rubi-none 
+#SBATCH --gres gpu:1 
+#SBATCH -o ../../../checkpoints/vqacp_topk-1500_lx-lstm_unfreeze-heads_loss-default_lr-6e-6_rubi-none.out
+
+cd ../../..
+source venvs/a_vs_c/bin/activate
+export PYTHONBREAKPOINT=ipdb.set_trace
+python main.py \
+    --jobname vqacp_topk-1500_lx-lstm_unfreeze-heads_loss-default_lr-6e-6_rubi-none \
+    --dataset vqacp \
+    --topk 1500 \
+    --model lx-lstm \
+    --loss default \
+    --epochs 100 \
+    --bsz 64 \
+    --val_bsz 64 \
+    --device 0 \
+    --unfreeze heads \
+    --num_workers 4 \
+    --lr 6e-6 \
+    --rubi none \
+    --wandb 
