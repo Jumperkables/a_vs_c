b60cb77b842f8943d06f2d56b10eed98d979b5ba
Jumperkables <tom.winterbottom@msn.com>
2022-05-30 00:19:13 +0100

    update scripts again

--- scripts/vqacp/runs/nc_and_minocc_test/lxmert_mao-9_normonly-expanded-nc-pt4-avsc.sh
+++ scripts/vqacp/runs/nc_and_minocc_test/lxmert_mao-9_normonly-expanded-nc-pt4-avsc.sh
@@ -5,24 +5,24 @@
 #SBATCH -t 2-00:00
 #SBATCH --mem 12G
 #SBATCH -p res-gpu-small
-#SBATCH --job-name vqacp_mao-9_normonly-expanded-nc-pt4_lxmert_loss-avsc_lr-4e-6_rubi-none 
+#SBATCH --job-name vqacp_mao-9_normonly-expanded-nc-pt4_lxmert_loss-avsc_lr-1e-6_rubi-none 
 #SBATCH --gres gpu:1 
-#SBATCH -o ../../../../checkpoints/vqacp_mao-9_normonly-expanded-nc-pt4_lxmert_loss-avsc_lr-4e-6_rubi-none.out
+#SBATCH -o ../../../../checkpoints/vqacp_mao-9_normonly-expanded-nc-pt4_lxmert_loss-avsc_lr-1e-6_rubi-none.out
 
 cd ../../../..
 source venv/bin/activate
 export PYTHONBREAKPOINT=ipdb.set_trace
 python main.py \
-    --jobname vqacp_mao-9_normonly-expanded-nc-pt4_lxmert_loss-avsc_lr-4e-6_rubi-none \
+    --jobname vqacp_mao-9_normonly-expanded-nc-pt4_lxmert_loss-avsc_lr-1e-6_rubi-none \
     --dataset vqacp \
     --model lxmert \
     --loss avsc \
-    --epochs 75 \
+    --epochs 125 \
     --bsz 64 \
     --val_bsz 64 \
     --device 0 \
     --num_workers 4 \
-    --lr 4e-6 \
+    --lr 1e-6 \
     --rubi none \
     --min_ans_occ 9 \
     --wandb \
