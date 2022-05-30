a8b683759dcbcfcf21b08541315df1a999bb3a79
Jumperkables <tom.winterbottom@msn.com>
2022-05-30 12:38:07 +0100

    vqacp2 scripts to rerun too

--- scripts/vqacp2/runs/nc_and_minocc_test/lxmert_mao-9_normonly-expanded-nc-pt4-avsc-scaled.sh
+++ scripts/vqacp2/runs/nc_and_minocc_test/lxmert_mao-9_normonly-expanded-nc-pt4-avsc-scaled.sh
@@ -17,12 +17,12 @@ python main.py \
     --dataset vqacp2 \
     --model lxmert \
     --loss avsc-scaled \
-    --epochs 75 \
+    --epochs 125 \
     --bsz 64 \
     --val_bsz 64 \
     --device 0 \
     --num_workers 4 \
-    --lr 3e-6 \
+    --lr 1e-6 \
     --rubi none \
     --min_ans_occ 9 \
     --wandb \
